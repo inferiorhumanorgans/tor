53d6e82e34fdb5fa8f6141aff14b81c4716808c5
Luis Hurtado <luis@time4code.com>
2009-02-27 09:52:03 -0500

    Change module_eval to class_eval to enable ActionView::Base as Tenjin context.

--- init.rb
+++ init.rb
@@ -2,5 +2,5 @@ require 'tenjin'
 
 ActionView::Template.register_template_handler :tenjin, TenjinHandler
 
-ActionView::Base.module_eval { include Tenjin::ContextHelper }
+ActionView::Base.class_eval { include Tenjin::ContextHelper }
 
