ce4c3568682365a55e39037be4589cdc8d917af4
Luis Hurtado <luis@time4code.com>
2009-03-24 20:55:50 -0500

    Add template's current directory as path so the import method doesn't complain.

--- lib/tenjin_handler.rb
+++ lib/tenjin_handler.rb
@@ -2,7 +2,9 @@ class TenjinHandler < ActionView::TemplateHandler
   include ActionView::TemplateHandlers::Compilable
 
   def compile(template)
-    "Tenjin::Engine.new.render('#{template.filename}',self)"
+    directory = File.dirname(template.filename)
+    filename = File.basename(template.filename)
+    "Tenjin::Engine.new(:path => ['#{directory}']).render('#{filename}',self)"
   end
   
 end
