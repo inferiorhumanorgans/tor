79d11de3a046105d902363f8e97934cf104db91c
Luis Hurtado <luis@time4code.com>
2009-08-24 01:10:50 -0500

    Fix missing final slash in cache directory path.

--- lib/tenjin_handler.rb
+++ lib/tenjin_handler.rb
@@ -4,7 +4,7 @@ class TenjinHandler < ActionView::TemplateHandler
   def compile(template)
     directory = File.dirname(template.filename)
     filename = File.basename(template.filename)
-    cache_directory = "#{RAILS_ROOT}#{File::SEPARATOR}tmp#{File::SEPARATOR}cache#{File::SEPARATOR}tenjin"
+    cache_directory = "#{RAILS_ROOT}#{File::SEPARATOR}tmp#{File::SEPARATOR}cache#{File::SEPARATOR}tenjin#{File::SEPARATOR}"
 
     "RailsTenjinEngine.new(:path => ['#{directory}'], :cache_dir => '#{cache_directory}').render('#{filename}',self)"
   end
