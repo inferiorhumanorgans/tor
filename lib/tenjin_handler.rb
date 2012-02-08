84fbf1a358288d583206aa656fe4579bdaa806d9
Alex Zepeda <alex@inferiorhumanorgans.com>
2012-02-08 10:49:51 -0800

    Update for Rail 3.0.

--- lib/tenjin_handler.rb
+++ lib/tenjin_handler.rb
@@ -2,8 +2,8 @@ class TenjinHandler < ActionView::TemplateHandler
   include ActionView::TemplateHandlers::Compilable
 
   def compile(template)
-    directory = File.dirname(template.filename)
-    filename = File.basename(template.filename)
+    directory = File.dirname(template.inspect)
+    filename = File.basename(template.inspect)
     cache_directory = "#{RAILS_ROOT}#{File::SEPARATOR}tmp#{File::SEPARATOR}cache#{File::SEPARATOR}tenjin#{File::SEPARATOR}"
 
     "RailsTenjinEngine.new(:path => ['#{directory}'], :cache_dir => '#{cache_directory}').render('#{filename}',self)"
